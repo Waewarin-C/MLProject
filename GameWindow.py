8871511f8715b007648c6e790f819e5458c1eb18
Gabriel-Morales <da.gabrielmora@gmail.com>
2020-10-20 21:22:21 -0500

    Added PyGame, beginning to test out UI drawing.

---
+++ GameWindow.py
@@ -0,0 +1,37 @@
+import os, sys
+import pygame
+from pygame.locals import *
+
+
+class GameWindow:
+    window_width = 600
+    window_height = 600
+
+    def __init__(self):
+        pygame.init()
+
+        self.window_size = (GameWindow.window_width, GameWindow.window_height)
+        self.screen = pygame.display.set_mode(self.window_size)
+        self.background = pygame.Surface(self.screen.get_size())
+        self.background = self.background.convert()
+        self.background.fill((250, 250, 250))
+        self.custom_cursor = pygame.image.load(os.path.join('./', 'macmouse.png')).convert_alpha()
+
+        pygame.display.set_caption("ML Tic Tac Toe")
+
+    def render_mac_mouse(self):
+        if sys.platform == "darwin":
+            self.custom_cursor = pygame.transform.scale(self.custom_cursor, (18, 27))
+            pygame.mouse.set_visible(False)
+            self.screen.blit(self.custom_cursor, (pygame.mouse.get_pos()))
+            pygame.display.flip()
+
+    def start_game(self):
+        while True:
+            self.screen.blit(self.background, (0, 0))
+            pygame.display.flip()
+            self.render_mac_mouse()
+
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    return
--- main.py
+++ main.py
@@ -1,36 +1,5 @@
-import os, sys
-import pygame
-from pygame.locals import *
-from FirstTestRun import *
-
-# Testing UI configurations
-def begin_game():
-
-    while True:
-
-        pygame.init()
-        window_size = (600, 600)
-        screen = pygame.display.set_mode(window_size)
-        pygame.display.set_caption("ML Tic Tac Toe")
-
-        background = pygame.Surface(screen.get_size())
-        background = background.convert()
-        background.fill((250, 250, 250))
-
-        screen.blit(background, (0, 0))
-        pygame.display.flip()
-
-        if sys.platform == "darwin":
-            custom_cursor = pygame.image.load(os.path.join('./', 'macmouse.png')).convert_alpha()
-            custom_cursor = pygame.transform.scale(custom_cursor, (18, 27))
-            pygame.mouse.set_visible(False)
-            screen.blit(custom_cursor, (pygame.mouse.get_pos()))
-            pygame.display.flip()
-
-        for event in pygame.event.get():
-            if event.type == QUIT:
-                return
-
+import GameWindow
 
 if __name__ == '__main__':
-    begin_game()
+    game = GameWindow.GameWindow()
+    game.start_game()
