5375f2df40e88e690226d827cc9c3a63811e6510
Gabriel-Morales <da.gabrielmora@gmail.com>
2020-12-01 13:10:14 -0600

    UPdated proj

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 from GameWindow import *
 
 if __name__ == '__main__':
+    icn = pygame.image.load('icn2.png')
+    pygame.display.set_icon(icn)
+
+    pygame.mixer.init()
+    pygame.mixer.music.load('music.mp3')
+    pygame.mixer.music.play(-1)
+    pygame.mixer.music.set_volume(.1)
+
     game = GameWindowFoundation()
     game.start_game()
